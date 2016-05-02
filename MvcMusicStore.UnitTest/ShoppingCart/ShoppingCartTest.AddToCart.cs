b7a6c4725f31e74b9b27f4b21c2a95f41b2af432
Bruno Tagliapietra <bruno.tagliapietra@xero.com>
2016-05-02 17:13:13 +1200

    When_CartItemIsNotFound_Then_StoreShouldContainANewCartItem

--- MvcMusicStore.UnitTest/ShoppingCart/ShoppingCartTest.AddToCart.cs
+++ MvcMusicStore.UnitTest/ShoppingCart/ShoppingCartTest.AddToCart.cs
@@ -1,5 +1,9 @@
 ﻿using System;
+using System.Data.Entity;
+using System.Linq;
+using FakeDbSet;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
 using MvcMusicStore.Models;
 
 namespace MvcMusicStore.UnitTest
@@ -7,9 +11,20 @@ namespace MvcMusicStore.UnitTest
     [TestClass]
     public class ShoppingCartTest_AddToCart
     {
-        private IMusicStoreEntities _store;
+        private IMusicStoreEntities Store
+        {
+            get { return _mockStore.Object; }
+        }
+
+        private Mock<IMusicStoreEntities> _mockStore;
+
         private Album _album;
-        private int _albumId = 42;
+        private const int _albumId = 42;
+
+        private readonly string _shoppingCartId = Guid.NewGuid().ToString();
+
+        private IDbSet<Cart> _carts;
+        private Cart _addedCart;
 
         [TestInitialize]
         public void Setup()
@@ -19,12 +34,40 @@ namespace MvcMusicStore.UnitTest
                 AlbumId = _albumId
             };
 
+            _addedCart = new Cart();
 
+            _carts = new InMemoryDbSet<Cart>();
+
+            _mockStore = new Mock<IMusicStoreEntities>();
+            _mockStore.Setup(store => store.Carts).Returns(_carts);
+        }
+
+        [TestMethod]
+        public void When_CartItemIsNotFound_Then_StoreShouldContainANewCartItem()
+        {
+            Setup_CartsWillBeEmpty();
+
+            CallSut();
+
+            _mockStore.VerifyGet(store => store.Carts, Times.Exactly(2));
+            _mockStore.Verify(store => store.SaveChanges(), Times.Once());
+
+            Assert.AreEqual(1, Store.Carts.Count(), string.Format("Store contains {0} objects instead of 1", Store.Carts.Count()));
+            Assert.AreNotEqual(_addedCart, Store.Carts.Single());
+        }
+
+        private void Setup_CartsWillBeEmpty()
+        {
+        }
+
+        private void Setup_CartsWillContainCurrentCart()
+        {
+            _carts.Add(_addedCart);
         }
 
         private void CallSut()
         {
-            var sut = new ShoppingCart(_store);
+            var sut = ShoppingCartFakeFactory.GetCart(_shoppingCartId, Store);
             sut.AddToCart(_album);
         }
     }
