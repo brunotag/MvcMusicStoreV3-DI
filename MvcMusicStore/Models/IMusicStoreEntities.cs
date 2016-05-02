3dc80b393c8f80296442e552059c98b5cf7f2a5e
Bruno Tagliapietra <bruno.tagliapietra@xero.com>
2016-05-02 13:48:35 +1200

    Create and implement IMusicStoreEntities

---
+++ MvcMusicStore/Models/IMusicStoreEntities.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+
+namespace MvcMusicStore.Models
+{
+    interface IMusicStoreEntities
+    {
+        IDbSet<Album> Albums { get; set; }
+        IDbSet<Genre> Genres { get; set; }
+        IDbSet<Artist> Artists { get; set; }
+        IDbSet<Cart> Carts { get; set; }
+        IDbSet<Order> Orders { get; set; }
+        IDbSet<OrderDetail> OrderDetails { get; set; }
+    }
+}
--- MvcMusicStore/Models/MusicStoreEntities.cs
+++ MvcMusicStore/Models/MusicStoreEntities.cs
@@ -2,13 +2,13 @@
 
 namespace MvcMusicStore.Models
 {
-    public class MusicStoreEntities : DbContext
+    public class MusicStoreEntities : DbContext, IMusicStoreEntities
     {
-        public DbSet<Album> Albums { get; set; }
-        public DbSet<Genre> Genres { get; set; }
-        public DbSet<Artist> Artists { get; set; }
-        public DbSet<Cart> Carts { get; set; }
-        public DbSet<Order> Orders { get; set; }
-        public DbSet<OrderDetail> OrderDetails { get; set; }
+        public IDbSet<Album> Albums { get; set; }
+        public IDbSet<Genre> Genres { get; set; }
+        public IDbSet<Artist> Artists { get; set; }
+        public IDbSet<Cart> Carts { get; set; }
+        public IDbSet<Order> Orders { get; set; }
+        public IDbSet<OrderDetail> OrderDetails { get; set; }
     }
 }
