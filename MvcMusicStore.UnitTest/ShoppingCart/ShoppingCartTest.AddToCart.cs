6165ef7b5954cc016044143d43f9421d5614f4b9
Bruno Tagliapietra <bruno.tagliapietra@xero.com>
2016-05-02 16:06:21 +1200

    Add ShoppingCartTest.AddToCart skeleton

---
+++ MvcMusicStore.UnitTest/ShoppingCart/ShoppingCartTest.AddToCart.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MvcMusicStore.Models;
+
+namespace MvcMusicStore.UnitTest
+{
+    [TestClass]
+    public class ShoppingCartTest_AddToCart
+    {
+        private IMusicStoreEntities _store;
+        private Album _album;
+        private int _albumId = 42;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _album = new Album()
+            {
+                AlbumId = _albumId
+            };
+
+
+        }
+
+        private void CallSut()
+        {
+            var sut = new ShoppingCart(_store);
+            sut.AddToCart(_album);
+        }
+    }
+}
