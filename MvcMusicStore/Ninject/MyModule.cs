9f64f2fbca87c51bb73a323e98c616221d9bc4be
Bruno Tagliapietra <bruno.tagliapietra@xero.com>
2016-05-17 18:32:04 +1200

    Add empty MyModule

---
+++ MvcMusicStore/Ninject/MyModule.cs
@@ -0,0 +1,16 @@
+﻿using Ninject.Modules;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcMusicStore.Ninject
+{
+    public class MyModule : NinjectModule
+    {
+        public override void Load()
+        {
+            
+        }
+    }
+}
